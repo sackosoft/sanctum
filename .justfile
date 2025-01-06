5a5457faaf13f60c013227afac827d9044745f6e
Theodore Sackos <theodore@sackosoft.com>
2025-01-06 09:27:38 -0800

    Refactor regression tests to support freezing the current output; add 'just freeze' target

--- .justfile
+++ .justfile
@@ -6,9 +6,11 @@ regression_suite := shell(abs, "./src/tests/regression-tests/")
 
 sanctum := shell(abs, "./zig-out/bin/sanctum")
 
-test: build (_test regression_tester sanctum regression_suite)
-_test script executable suite:
-    @python3 {{script}} {{executable}} {{suite}}
+test: build (_test regression_tester sanctum regression_suite "--test")
+freeze: build (_test regression_tester sanctum regression_suite "--freeze")
+_test script executable suite flags:
+    @python3 {{script}} {{executable}} {{suite}} {{flags}}
+
 
 build:
     zig build
--- src/tests/regression.py
+++ src/tests/regression.py
@@ -47,41 +47,70 @@ def try_run_process(command: list):
         exit(1)
     return result
 
+def _assert_outputs(test_dir_path, result):
+    """
+    Attempts to load the `stdout.assert` and `stderr.assert` files for the given test case,
+    and compares them against the actual output from the program. If the expected output
+    does not match the actual output, the program is halted with an error: a test case failed.
+    """
+    assertion_files = [
+        (TestFiles.STDOUT, result.stdout),
+        (TestFiles.STDERR, result.stderr),
+    ]
+    for (assert_file_name, actual_bytes) in assertion_files:
+        assert_file = t / assert_file_name
+        if assert_file.is_file():
+            expected = assert_file.read_text()
+            assert_results_mach_expected(expected, actual_bytes)
+
+def _freeze_outputs(test_dir_path, result):
+    """
+    As breaking changes and refactorings are introduced to sanctum, it's common to find that
+    the new actual output does not match the saved expected output files. In such a case, after
+    verifying that the new output is correct, we can "freeze" those actual outputs to the expected
+    output files. This sets the current output from Sanctum as the "golden output" for future tests.
+    """
+    assertion_files = [
+        (TestFiles.STDOUT, result.stdout),
+        (TestFiles.STDERR, result.stderr),
+    ]
+    for (assert_file_name, actual_bytes) in assertion_files:
+        actual = try_decode_utf8(actual_bytes)
+
+        assert_file = t / assert_file_name
+        assert_file.write_text(actual)
+
+TEST_ACTIONS = {
+    "--test": _assert_outputs,
+    "--freeze": _freeze_outputs,
+}
+
 if __name__ == "__main__":
-    if len(argv) != 3:
+    if len(argv) != 4:
         file_name = Path(__file__).name
-        print(f"Invalid arguments. Expected usage: `python {file_name} <path_to_executable_to_test> <path_to_test_suite_directory>`")
+        print(f"Invalid number of arguments.\n    Usage: `python {file_name} <path_to_executable_to_test> <path_to_test_suite_directory> [--test|--freeze]`")
         exit(1)
 
-    exe = argv[1]
+    [_, exe, test_suite_dir, action_arg] = argv
     if not os.access(exe, os.X_OK):
         print(f"Unable to run program, '{argv[1]}' is not executable.")
         exit(1)
 
-    test_suite_dir = Path(argv[2])
+    test_suite_dir = Path(test_suite_dir)
     if not test_suite_dir.is_dir():
         print(f"Invalid test suite: '{argv[2]}', expected a path to a directory.")
         exit(1)
 
     tests = [Path(directory[0]) for directory in os.walk(str(test_suite_dir.resolve())) if TestFiles.SPELL in directory[2]]
     for t in tests:
-        print(f"Running regression test '{t}'")
+        print(f"Running regression {action_arg.replace('-', '')} on '{t}'")
         event_seed = t / TestFiles.EVENT_SEED
         spell = t / TestFiles.SPELL
         command = [exe, "cast", str(spell.resolve()), "--seed", str(event_seed.resolve())]
 
         result = try_run_process(command)
-
-        assertion_files = [
-            (TestFiles.STDOUT, result.stdout),
-            (TestFiles.STDERR, result.stderr),
-        ]
-        for (assert_file_name, actual_bytes) in assertion_files:
-            assert_file = t / assert_file_name
-            if assert_file.is_file():
-                expected = assert_file.read_text()
-                assert_results_mach_expected(expected, actual_bytes)
+        test_action = TEST_ACTIONS[action_arg]
+        test_action(t, result)
 
     print("PASS")
     exit(0)
-
