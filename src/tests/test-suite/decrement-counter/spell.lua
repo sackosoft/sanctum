04e40cd5e9ca5caed3e5510acff26f1cf4963d11
Theodore Sackos <theodore@sackosoft.com>
2025-01-11 17:37:12 -0800

    Benchmark: 1k iterations, 1.455ms average (range [0.960, 4.532]ms) - Lua - No serialization, single setup, empty spell

--- src/tests/test-suite/decrement-counter/spell.lua
+++ src/tests/test-suite/decrement-counter/spell.lua
@@ -1,13 +1,6 @@
 local decrement_counter = {
     cast = function(event)
-        if event.counter <= 1 then
-            -- print(string.format("The counter has reached 0, stopping.", event.counter))
-            return nil
-        end
-
-        event.counter = event.counter - 1
-        -- print(string.format("The counter is now %d.", event.counter))
-        return event
+        return nil;
     end,
 }
 return decrement_counter
