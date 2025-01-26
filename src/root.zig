2f022254af4759472edf6704f7e6e6cff2191cc1
Theodore Sackos <theodore@sackosoft.com>
2025-01-25 18:56:23 -0800

    License header

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,4 @@
-// Copyright (c) 2024-2025 Theodore Sackos
-// SPDX-License-Identifier: AGPL-3.0-or-later
+//! Copyright (c) 2024-2025 Theodore Sackos
+//! SPDX-License-Identifier: AGPL-3.0-or-later
 
 const std = @import("std");
--- src/zlmp.zig
+++ src/zlmp.zig
@@ -1,5 +1,5 @@
-// Copyright (c) 2024-2025 Theodore Sackos
-// SPDX-License-Identifier: AGPL-3.0-or-later
+//! Copyright (c) 2024-2025 Theodore Sackos
+//! SPDX-License-Identifier: AGPL-3.0-or-later
 
 const std = @import("std");
 const ArrayList = std.ArrayList;
